010978c83ef74379b782106793656c58a4d12d17
Nathan Wigen <nathanwigen@Nathans-MacBook-Air.local>
2020-10-10 16:10:42 -0700

    Added Login Screen

--- review-the-game/src/App.js
+++ review-the-game/src/App.js
@@ -1,17 +1,65 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import axios from "axios"
+// import './App.css';
+// import { baseUrl } from './constants';
+// import CreateReview from './CreateReview'
+
+// function App() {
+  // const [reviews, setReview] = useState([])
+  // const [fetchReviews, setFetchReviews] = useState(false)
+
+  // useEffect(() => {
+  //   const getReview = async() => {
+  //     const airtableURL = `${baseUrl}`
+  //     const resp = await axios.get(airtableURL, {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+  //       },
+  //     })
+  //     setReview(resp.data.records)
+  //   }
+  //   getReview()
+  // },[fetchReviews])
+  
+//   return (
+//     <div className="App">
+//       <h1>Review Your Games</h1>
+//       <CreateReview/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//This code is from Soleli.
+
+import React, { useEffect, useState } from "react";
 import axios from "axios"
-import './App.css';
+import {
+  BrowserRouter as Route,
+  Switch,
+  Link,
+  useHistory
+} from "react-router-dom";
+// import CreateReview from './CreateReview'
 import { baseUrl } from './constants';
-import CreateReview from './CreateReview'
+import Review from "./Review"
+import "./App.css";
+
 
 function App() {
   const [reviews, setReview] = useState([])
   const [fetchReviews, setFetchReviews] = useState(false)
+  const [username, setUsername] = useState(localStorage.getItem("username"));
+  const [usernameInput, setUsernameInput] = useState("");
+  const [userPassword, setUserPassword] = useState(localStorage.getItem("userPassword"));
+  const [userPasswordInput, setUserPasswordInput] = useState("");
 
   useEffect(() => {
     const getReview = async() => {
-      const airtableURL = `${baseUrl}`
-      const resp = await axios.get(airtableURL, {
+      const airtableUrl = `${baseUrl}`
+      const resp = await axios.get(airtableUrl, {
         headers: {
           Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
         },
@@ -20,13 +68,43 @@ function App() {
     }
     getReview()
   },[fetchReviews])
-  
+
+  // const history = useHistory();
+  useEffect(() => {
+    if (username) {
+      // history.push('/home');
+    }
+  }, []);
+  const takeMeThere = () => {
+    localStorage.setItem("username", usernameInput);
+    setUsername(usernameInput);
+    localStorage.setItem("password", userPasswordInput);
+    setUserPassword(userPasswordInput);
+    // history.push("/home");
+  };
   return (
     <div className="App">
-      <h1>Review Your Games</h1>
-      <CreateReview/>
+      <header>Review Your Game</header>
+      <Route exact path="/">
+        <label>Username:</label>
+        <input
+          value={usernameInput}
+          onChange={(e) => setUsernameInput(e.target.value)}
+        />
+        <label>Password:</label>
+        <input
+          value={userPasswordInput}
+          onChange={(e) => setUserPasswordInput(e.target.value)}
+        />
+        <button onClick={takeMeThere}>Submit</button>
+      </Route>
+      <Route path="/review">
+        <h2>{username}</h2>
+        <Review />
+      </Route>
     </div>
   );
 }
+
 
 export default App;
---
+++ review-the-game/src/Review.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react"
+import axios from "axios"
+import CreatReview from "./CreateReview"
+import { baseUrl } from "./constants"
+
+
+function Review(props) {
+  const [deleted, setDeleted] = useState(false)
+
+  const handleDelete = async () => {
+    setDeleted(true)
+    setTimeout(async () => {
+      const airtableUrl = `${baseUrl}/reviews/${props.review.id}`
+      await axios.delete(airtableUrl)
+      props.fetchReview((prevFetchReviews => !prevFetchReviews))
+      setDeleted(false)
+    }, 1500)
+  }
+
+
+return (
+  <div className="review">
+    <h3>{props.review.fields.title}</h3>
+    <h4>{props.review.fields.text}</h4>
+    <h5>{props.review.fields.author}</h5>
+    <button onClick={handleDelete}>{deleted ? "Delete" : "Deleted"}</button>
+    {/* <UpdateReview
+      review={props.review}
+      fetchReviews={props.fetchReviews}
+      setFetchReviews={props.setFetchReviews}
+    /> */}
+  </div>
+);
+}
+
+export default Review
